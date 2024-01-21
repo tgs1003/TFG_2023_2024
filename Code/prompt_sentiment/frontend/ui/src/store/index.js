70c932b4852667942c91e98cf2edc1c33d662132
Ricardo Garcia <94012220+tgs1003@users.noreply.github.com>
2024-01-21 10:22:19 +0100

    Update index.js

--- Code/prompt_sentiment/frontend/ui/src/store/index.js
+++ Code/prompt_sentiment/frontend/ui/src/store/index.js
@@ -6,54 +6,53 @@ import TokenService from "../services/token.service";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-state: {
-    status: "",
-    token: TokenService.getLocalAccessToken(),
-    user: {},
-    dark:true
-},
-mutations: {
-    auth_request(state) {
-        state.status = "loading";
+    state: {
+        status: "",
+        token: TokenService.getLocalAccessToken(),
+        user: {},
+        dark: true
     },
-    auth_success(state, token, user) {
-        state.status = "success";
-        state.token = token;
-        state.user = user;
+    mutations: {
+        auth_request(state) {
+            state.status = "loading";
+        },
+        auth_success(state, token, user) {
+            state.status = "success";
+            state.token = token;
+            state.user = user;
+        },
+        auth_error(state) {
+            state.status = "error";
+        },
+        logout(state) {
+            state.status = "";
+            state.token = "";
+        },
     },
-    auth_error(state) {
-        state.status = "error";
-    },
-    logout(state) {
-        state.status = "";
-        state.token = "";
-    },
-},
-actions: {
-    login({commit}, user) {
-        commit("auth_request");
-        return api.post("/auth/login", user)
-        .then((response)=> {
-            if(response.data.access_token){
-                TokenService.setUser(response.data);
-                commit("auth_success", response.data.access_token, user.email);
-            }
-            return response;
-        })
-        .catch(err => {
-                
-            TokenService.removeUser();
-            console.info(err);
-            return err;
-        });
-    },
-    register({commit},user)
-    {
-        commit("auth_request");
-        return api.post("/auth/register", user)
-                .then((response)=> {
-                if (response.code === 200)
-                    this.$router.push("/")
+    actions: {
+        login({ commit }, user) {
+            commit("auth_request");
+            return api.post("/auth/login", user)
+                .then((response) => {
+                    if (response.data.access_token) {
+                        TokenService.setUser(response.data);
+                        commit("auth_success", response.data.access_token, user.email);
+                    }
+                    return response;
+                })
+                .catch(err => {
+
+                    TokenService.removeUser();
+                    console.info(err);
+                    return err;
+                });
+        },
+        register({ commit }, user) {
+            commit("auth_request");
+            return api.post("/auth/register", user)
+                .then((response) => {
+                    if (response.code === 200)
+                        this.$router.push("/")
                     return response;
                 })
                 .catch(err => {
@@ -62,18 +61,18 @@ actions: {
                     return err;
                 });
 
-    },
-    logout({ commit }) {
-    return new Promise((resolve) => {
-        commit("logout");
-        TokenService.removeUser();
-        commit("auth_success", "", "")
-        resolve();
-        })
+        },
+        logout({ commit }) {
+            return new Promise((resolve) => {
+                commit("logout");
+                TokenService.removeUser();
+                commit("auth_success", "", "")
+                resolve();
+            })
         },
     },
-getters : {
-    isLoggedIn: state => !!state.token,
-    authStatus: state => state.status,
-  },
+    getters: {
+        isLoggedIn: state => !!state.token,
+        authStatus: state => state.status,
+    },
 })

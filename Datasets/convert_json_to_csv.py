6a237a38ca96ab6ea482bd0309364dd07ace84b1
Ricardo Garcia <94012220+tgs1003@users.noreply.github.com>
2024-02-03 13:16:06 +0100

    Organización datasets

---
+++ Datasets/convert_json_to_csv.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import json
+import os
+def convert(filename):
+    # Reading JSON data from a file
+    with open(filename) as f:
+        json_data = f.read()
+
+    # Converting JSON data to a pandas DataFrame
+    df = pd.read_json(json_data)
+
+    # Writing DataFrame to a CSV file
+    df.to_csv(filename + ".csv", index=False)
+    
+
+files = [f for f in os.listdir() if not os.path.isdir(f) and f.endswith('json')]
+for file in files:
+    convert(file)
